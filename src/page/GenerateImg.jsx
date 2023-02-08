77607e01fe033bce98b97524383eabd7886b0731
houdamzari <houdamzari1999@gmail.com>
2023-02-08 11:16:16 +0100

    Deploy backend

--- src/page/GenerateImg.jsx
+++ src/page/GenerateImg.jsx
@@ -21,15 +21,18 @@ const GenerateImg = () => {
     setGeneratingImg(true);
 
     try {
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt: form.prompt,
-        }),
-      });
+      const response = await fetch(
+        "https://picture-ai.onrender.com/api/v1/dalle",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt: form.prompt,
+          }),
+        }
+      );
 
       const data = await response.json();
       setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
@@ -50,13 +53,16 @@ const GenerateImg = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:8080/api/v1/post", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ ...form }),
-      });
+      const response = await fetch(
+        "https://picture-ai.onrender.com/api/v1/post",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ ...form }),
+        }
+      );
 
       await response.json();
       alert("Success");
